0849ce789f9024a5b20e5c71a4598f2acd4a745d
VEMPADAPU KARTHIK <karthikvempadapu1994@gmail.com>
2025-03-25 13:22:56 +0530

    Update sanitize.java

--- sanitize.java
+++ sanitize.java
@@ -5,5 +5,7 @@ public String sanitizeJson(String json) {
     // Remove the specific characters '?' and '~'
     json = json.replaceAll("[\\?~]", "");
 
+json = json.replaceAll("[\\u0000-\\u001F\\u0085\\u0028\\u0029]", "");
+
     return json;
 }
